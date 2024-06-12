15f2c127d890e3e0908c3a637c55eca562c13a2b
Freanadira <freanadirasolihah@gmail.com>
2024-06-12 15:48:30 +0700

    praktikum 13

---
+++ routes/web copy.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
+
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
-use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//menampilkan teks salam
-Route::get('/salam',function(){
-    return "Assalamualaikum....";
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified']);
 
-//routing profil
-Route::get('/profile',function(){
-    return view('profile');
-});
-
-Route::get('admin/dashboard', [DashboardController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 //route untuk menampilkan student
 Route::get('admin/student', [StudentController::class, 'index']);
@@ -58,3 +56,9 @@ Route::put('admin/course/update/{id}', [CourseController::class, 'update']);
 
 //untuk menghapus course
 Route::delete('admin/course/delete/{id}', [CourseController::class, 'destroy']);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
